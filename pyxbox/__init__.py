2121d89ce755a77dad051df2de2420efcd1419bd
brucelong <18656170559@163.com>
2022-01-18 12:00:50 +0800

    初始化项目

---
+++ pyxbox/__init__.py
@@ -0,0 +1,8 @@
+# #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2022/1/18 12:00
+# @Author : BruceLong
+# @FileName: __init__.py.py
+# @Email   : 18656170559@163.com
+# @Software: PyCharm
+# @Blog ：http://www.cnblogs.com/yunlongaimeng/
